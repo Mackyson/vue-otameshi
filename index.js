1b6d26f3d920c31bf0075897113d5d3cb206e69d
Mackyson <mackyson72@gmail.com>
2021-01-11 03:26:13 +0900

    vue-chartjsによる表示(のためにvue2.xへダウングレード)

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const App = Vue.createApp(
-	{
-		data() {
-			return {
-				message: "Hello",
-				count: 0,
-				todos:[
-					{id: 0,text: "a"},
-					{id: 1,text: "b"}
-				],
-			}
-		},
-		methods:{
-			countup: function(){
-				this.count++
-			}
-		}
-	}
-)
-App.component("todo-list",
+Vue.component("todo-list",
 	{
 		props: ["todo"],
 		template: "<li>{{todo.text}}</li>"
 	}
 )
-App.mount("#app")
+Vue.component('line-chart', {
+  extends: VueChartJs.Line,
+  mounted () {
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 39, 10, 40, 39, 80, 40]
+        }
+      ]
+    },
+	{
+		responsive: true,
+		maintainAspectRatio: false
+	})
+  }
+})
+const App = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello',
+		count: 0,
+		todos:[
+			{id: 0,text: "a"},
+			{id: 1,text: "b"},
+		]
+	},
+	methods:{
+		countup: function(){
+			this.count++
+		}
+	}
+})
+
