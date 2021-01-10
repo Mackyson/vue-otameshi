8d3fe363eca4babbf14ee35a686fc885125c2297
Mackyson <mackyson72@gmail.com>
2021-01-10 18:52:23 +0900

    yarnで管理

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const App = {
-	data() {
-		return {
-			count: 0,
-			message: "Hello",
-			todos:[
-				{id: 0, text:"a"},
-				{id: 1, text:"b"}
-			]
-		}
-	},
-	methods :{
-		countup: function(){
-			this.count++
+const App = Vue.createApp(
+	{
+		data() {
+			return {
+				message: "Hello",
+				count: 0,
+				todos:[
+					{id: 0,text: "a"},
+					{id: 1,text: "b"}
+				],
+			}
+		},
+		methods:{
+			countup: function(){
+				this.count++
+			}
 		}
 	}
-}
-app = Vue.createApp(App)
-app.component("todo-item",{
-	props: ["todo"],
-	template: "<li>{{todo.text}}</li>"
-})
-
-app.mount("#app")
+)
+App.component("todo-list",
+	{
+		props: ["todo"],
+		template: "<li>{{todo.text}}</li>"
+	}
+)
+App.mount("#app")
